9509f93c9fa6da31be33eb3f6872fa10372dc816
yuji y united <yuji-yamagata@united.jp>
2016-04-19 23:46:53 +0000

    first commit?

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,7 +14,43 @@ class User < ActiveRecord::Base
 
   has_many :ownerships , foreign_key: "user_id", dependent: :destroy
   has_many :items ,through: :ownerships
-
+  has_many :wants, class_name: "Want", foreign_key: "user_id", dependent: :destroy
+  has_many :want_items , through: :wants, source: :item
+  has_many :haves, class_name: "Have", foreign_key: "user_id", dependent: :destroy
+  has_many :have_items , through: :haves, source: :item
+  
+  
+  # itemをwantする
+  def want(item)
+      want_items.find_or_create_by(item_id: item.id)
+  end
+    
+  # itemのwantを解除する
+  def unwant(item)
+      want_item = want_items.find_by(item_id: item.id)
+      want_item.destroy if want_item
+  end
+    
+  # あるitemをwantしているかどうか
+  def want?(item)
+      want_items.include?(item)
+  end
+  
+  # itemをhaveする
+  def have(item)
+      have_items.find_or_create_by(item_id: item.id)
+  end
+    
+  # itemのhaveを解除する
+  def unhave(item)
+      have_item = have_items.find_by(item_id: item.id)
+      have_item.destroy if have_item
+  end
+    
+  # あるitemをwantしているかどうか
+  def have?(item)
+      have_items.include?(item)
+  end
 
   # 他のユーザーをフォローする
   def follow(other_user)
