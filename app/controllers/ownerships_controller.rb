2a33bb8db8ee55e85d66590f3f05803bcc26b454
yuji y united <yuji-yamagata@united.jp>
2016-04-22 10:05:36 +0000

    fix

--- app/controllers/ownerships_controller.rb
+++ app/controllers/ownerships_controller.rb
@@ -12,7 +12,7 @@ class OwnershipsController < ApplicationController
     if @item.new_record?
       begin
         # TODO 商品情報の取得 Amazon::Ecs.item_lookupを用いてください
-        response = Amazon::Ecs.item_lookup(params[:asin], country: 'jp')
+        response = Amazon::Ecs.item_lookup(params[:asin], response_group: 'Medium',  country: 'jp')
       rescue Amazon::RequestError => e
         return render :js => "alert('#{e.message}')"
       end
